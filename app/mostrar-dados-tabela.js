d5366edb8fc162ba19b820bef70ea4ff5db8412d
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2024-09-03 15:58:24 -0300

    Adição das páginas entrega e doação

--- app/mostrar-dados-tabela.js
+++ app/mostrar-dados-tabela.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('/database/dados-cesta-teste.json')
+    fetch('/database/estoque.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Erro ao carregar o arquivo JSON');
